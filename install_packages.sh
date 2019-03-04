9af66e1ba217c795a1d3e2697f93f00933c76459
Dan Reed <dreed@pinterest.com>
2019-03-04 15:26:47 -0800

    install everything i hope

--- install_packages.sh
+++ install_packages.sh
@@ -1,2 +1,6 @@
+echo "Installing quiz report dependencies"
 yarn --cwd ./quiz-report install
+echo "Installing quiz client dependencies"
+yarn --cwd ./quiz-client install
+echo "Installing api server dependencies"
 pip install -r ./api_server/requirements.txt
