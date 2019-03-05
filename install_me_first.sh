898451adb0f9fdf730992e9864a7bf9bb9bd4ee5
Dan Reed <dreed@pinterest.com>
2019-03-05 13:24:15 -0800

    add sepreate scripts for node and yarn

--- install_me_first.sh
+++ install_me_first.sh
@@ -1,4 +1,2 @@
-echo "Installing node (not installed by default on a raspi zero)"
-wget -O - https://raw.githubusercontent.com/sdesalas/node-pi-zero/master/install-node-v8.10.0.sh | bash
-echo "Installing Yarn (needed for both servers)"
-curl -o- -L https://yarnpkg.com/install.sh | bash
+./install_node.sh
+./install_yarn.sh
