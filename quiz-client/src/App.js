ecb47f308029311592616a8fc297e93f4d4022e2
Dan Reed <dreed@pinterest.com>
2019-03-10 15:59:21 -0700

    app now manages state for the quiz: not started, ongoing, or complete

--- quiz-client/src/App.js
+++ quiz-client/src/App.js
@@ -8,16 +8,42 @@ class App extends Component {
     super(props);
     this.state = {
       error: null,
-      isLoaded: false
+      isLoaded: false,
+      quizCompletionState: QuizState.NotStarted
     };
+    this.quizBeganCallback = this.quizBeganCallback.bind(this);
+    this.quizCompletedCallback = this.quizCompletedCallback.bind(this);
+  }
+
+  quizBeganCallback() {
+    this.setState({quizCompletionState: QuizState.Ongoing})
+  }
+
+  quizCompletedCallback() {
+    this.setState({quizCompletionState: QuizState.Completed})
   }
 
   render() {
-    return (
-      <div className="App">
-        Hello world!
-      </div>
-    );
+    if (this.state.quizCompletionState == QuizState.NotStarted) {
+      return (
+        <div className="App">
+          <button onClick={this.quizBeganCallback}>Start the quiz</button>
+        </div>
+      );
+    } else if (this.state.quizCompletionState == QuizState.Ongoing) {
+      return (
+        <div className="App">
+          You began the quiz.
+          <button onClick={this.quizCompletedCallback}>Finish the quiz</button>
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          You finished!
+        </div>
+      );
+    }
   }
 }
 
