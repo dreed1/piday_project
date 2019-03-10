20af079836e1f9d0979c00f8cac6e4d9a3ce25ac
Dan Reed <dreed@pinterest.com>
2019-03-10 16:57:45 -0700

    we can now render the answer correctly and log a click

--- quiz-client/src/Answer.js
+++ quiz-client/src/Answer.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
 
 class Answer extends Component {
+	constructor(props) {
+    super(props);
+    console.log("Creating a new answer")
+    this.state = {
+      error: null,
+      isLoaded: false,
+      id: props.id,
+      question_id: props.question_id,
+      text: props.text,
+      value: props.value,
+      image_url: props.image_url
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log("I HAVE BEEN CLICKED, WOO!!!!")
+  }
+
 	render() {
-		return(
-			<div> I'm an answer, howdy!</div>
-		);
-	}
+    var textContent = <div className="AnswerText">{this.state.text}</div>;
+    var imageContent = null;
+    if (this.state.image_url) {
+      console.log(this.state.image_url)
+      imageContent = <img className="AnswerImage" alt={this.state.text} src={this.state.image_url}></img>
+    }
+    return (
+      <div className="QuizAnswer" onClick={this.handleClick}>
+        {textContent}
+        {imageContent}
+      </div>
+    );
+  }
 }
 
 export default Answer;
