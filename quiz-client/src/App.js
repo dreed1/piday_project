4090aa0ff9fdc20cca031d3f1d550106479ff905
Dan Reed <dreed@pinterest.com>
2019-03-10 16:16:02 -0700

    create a constants file, ask the API for my name, render what my name is when we have it.

--- quiz-client/src/App.js
+++ quiz-client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import QuizState from './enums';
 import NameRegistrar from './NameRegistrar';
 
 class App extends Component {
@@ -9,43 +8,17 @@ class App extends Component {
     super(props);
     this.state = {
       error: null,
-      isLoaded: false,
-      quizCompletionState: QuizState.NotStarted
+      isLoaded: false
     };
-    this.quizBeganCallback = this.quizBeganCallback.bind(this);
-    this.quizCompletedCallback = this.quizCompletedCallback.bind(this);
-  }
-
-  quizBeganCallback() {
-    this.setState({quizCompletionState: QuizState.Ongoing})
-  }
-
-  quizCompletedCallback() {
-    this.setState({quizCompletionState: QuizState.Completed})
   }
 
   render() {
-    if (this.state.quizCompletionState == QuizState.NotStarted) {
-      return (
-        <div className="App">
-          <NameRegistrar />
-          <button onClick={this.quizBeganCallback}>Start the quiz</button>
-        </div>
-      );
-    } else if (this.state.quizCompletionState == QuizState.Ongoing) {
-      return (
-        <div className="App">
-          You began the quiz.
-          <button onClick={this.quizCompletedCallback}>Finish the quiz</button>
-        </div>
-      );
-    } else {
-      return (
-        <div className="App">
-          You finished!
-        </div>
-      );
-    }
+    return (
+      <div className="App">
+        <NameRegistrar />
+        <div>I will be the quiz soon!</div>
+      </div>
+    );
   }
 }
 
