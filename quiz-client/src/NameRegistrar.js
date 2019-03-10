0df3a6577fbe081c74f88dd0af415c14aaeb81f5
Dan Reed <dreed@pinterest.com>
2019-03-10 16:08:16 -0700

    create a component for NameRegistrar, render a dummy div for it

---
+++ quiz-client/src/NameRegistrar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class NameRegistrar extends Component {
+	render() {
+		return (
+			<div> IM GONNA SHOW NAMES HERE. </div>
+		);
+	}
+}
+
+export default NameRegistrar;
