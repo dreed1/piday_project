18e38b3f2bee685df7bc2181f48e60a555a75cea
Dan Reed <dreed@pinterest.com>
2019-03-10 16:45:08 -0700

    create a quiz component, allow it to call the API

---
+++ quiz-client/src/Quiz.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import Confetti from 'react-confetti';
+
+import APILocation from './Constants';
+
+class Quiz extends Component {
+	constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      quizComplete: false,
+      questions: [],
+      currentQuestion: -1,
+      quizBeganCallback: this.props.quizBeganCallback,
+      quizCompletedCallback: this.props.quizCompletedCallback
+    };
+  }
+
+  getNextQuestion() {
+    const fetchURL = APILocation + "next_question";
+    this.setState({
+      isLoaded: false
+    })
+    fetch(fetchURL)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          if (result.question.length > 0) {
+            this.setState({
+              isLoaded: true,
+              questions: this.state.questions.concat([result]),
+              currentQuestion: this.state.currentQuestion + 1
+            });
+          } else {
+            console.log("You finished the quiz.")
+          }
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+	render() {
+    //if we didn't complete the quiz, and our current question index is >=0 we must be taking it currently.
+    if (!this.state.quizComplete && this.state.currentQuestion >= 0) {
+      return (
+      	<div>You are taking the quiz, and you'd see a question here normally but we didnt finish that yet.</div>
+      );
+    // if we don't have a good current question index, we must not have started it yet
+    } else if (!this.state.quizComplete) {
+      return (
+        <div>You havent yet begun to take the quiz.</div>
+      )
+    //and if we have set the quiz complete flag, we must be done -- let's celebrate.
+    } else {
+      return (
+        <div>
+          <Confetti />
+          <div className="QuizCompleteTitle">You finished the quiz</div>
+        </div>
+      );
+    }
+  }
+}
+
+export default Quiz;
