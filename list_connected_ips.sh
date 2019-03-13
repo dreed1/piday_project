68ebeaeb795a5ec4f432f1d5bd2cb808bbbb15e5
Dan Reed <dreed@pinterest.com>
2019-03-13 10:03:11 -0700

    add arp to the list connected ips script, much better than nmap

--- list_connected_ips.sh
+++ list_connected_ips.sh
@@ -1,4 +1,6 @@
 #this script uses nmap to tell you the mac address and IP of each computer connected on the network.
 
 #make sure you change the IP here to whatever the router says.
-sudo nmap -sn 192.168.29.1/24
+sudo nmap -sn 172.16.154.1/24
+
+#arp -a
